9722ee01fbb5213b66e7e013b8b865d1df34e043
medfordengineering <schristy@medford.k12.ma.us>
2024-11-08 15:18:11 -0500

    initial working with MCP

--- TypewriterESP32/TypewriterESP32.ino
+++ TypewriterESP32/TypewriterESP32.ino
@@ -6,7 +6,6 @@
 
 Adafruit_MCP23X17 mcp;
 
-
 uint8_t letter[128][2] = {
   { 6, 5 },  //0 NULL
   { 0, 0 },   //1 NULL
@@ -22,13 +21,14 @@ long timeLast;
 
 void setup() {
   Serial.begin(115200);
-
   if (!mcp.begin_I2C()) {
-    Serial.println("Error.");
-    while (1);
+    Serial.println("ER");
   }
-  for (int x = 0; x < 8; i++) {
-    mcp.pinMode(LED_PIN, INPUT_PULLUP);
+
+
+  Serial.println("found");
+  for (int x = 0; x < 8; x++) {
+    mcp.pinMode(x, INPUT_PULLUP);
   }
   pinMode(5, OUTPUT);
   pinMode(6, INPUT_PULLUP);
@@ -42,31 +42,31 @@ void loop() {
   timeThis = millis();
   timeLast = timeThis;
   if (digitalRead(7) == false) {
-    pinMode(8, OUTPUT);
-    
-    while (timeThis - timeLast <= 100) {
-     digitalWrite(5, mpc.digitalRead(0));
-     // digitalWrite(8, digitalRead(9));
-     // digitalWrite(5, digitalRead(6));
+    pinMode(5, OUTPUT);
+   // mcp.pinMode(x, INPUT_PULLUP);
+    while (timeThis - timeLast <= 50) {
+      digitalWrite(5, mcp.digitalRead(0));
+      // digitalWrite(8, digitalRead(9));
+      //  digitalWrite(5, digitalRead(6));
       timeThis = millis();
     }
-    digitalWrite(5, HIGH);
-    pinMode(8, INPUT);
-/*
-    delay(1000);
-    timeThis = millis();
-    timeLast = timeThis;
-    // pinMode(8, OUTPUT);
-    while (timeThis - timeLast <= 100) {
-      //  digitalWrite(8, digitalRead(9));
-      digitalWrite(5, digitalRead(6));
-      timeThis = millis();
-    }
-    digitalWrite(5, HIGH);
-    //  pinMode(8, INPUT);
-  }*/
+    // digitalWrite(5, HIGH);
+    pinMode(5, INPUT);
+    /*
+        delay(1000);
+        timeThis = millis();
+        timeLast = timeThis;
+        // pinMode(8, OUTPUT);
+        while (timeThis - timeLast <= 100) {
+          //  digitalWrite(8, digitalRead(9));
+          digitalWrite(5, digitalRead(6));
+          timeThis = millis();
+        }
+        digitalWrite(5, HIGH);
+        //  pinMode(8, INPUT);
+      }*/
 
-
+  }
 
 
 }
