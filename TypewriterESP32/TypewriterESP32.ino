4b7e8a42496747925f017e0c8ef4e6ac06878b05
sam <schristy@medford.k12.ma.us>
2024-11-08 13:27:44 -0500

    added MCP pin expander

--- TypewriterESP32/TypewriterESP32.ino
+++ TypewriterESP32/TypewriterESP32.ino
@@ -1,6 +1,10 @@
 
+#include <Adafruit_MCP23X17.h>
+
 #define PRESS   1
 #define NOPRESS 0
+
+Adafruit_MCP23X17 mcp;
 
 
 uint8_t letter[128][2] = {
@@ -19,6 +23,13 @@ long timeLast;
 void setup() {
   Serial.begin(115200);
 
+  if (!mcp.begin_I2C()) {
+    Serial.println("Error.");
+    while (1);
+  }
+  for (int x = 0; x < 8; i++) {
+    mcp.pinMode(LED_PIN, INPUT_PULLUP);
+  }
   pinMode(5, OUTPUT);
   pinMode(6, INPUT_PULLUP);
   pinMode(8, OUTPUT);
@@ -32,14 +43,16 @@ void loop() {
   timeLast = timeThis;
   if (digitalRead(7) == false) {
     pinMode(8, OUTPUT);
+    
     while (timeThis - timeLast <= 100) {
-      digitalWrite(8, digitalRead(9));
-      digitalWrite(5, digitalRead(6));
+     digitalWrite(5, mpc.digitalRead(0));
+     // digitalWrite(8, digitalRead(9));
+     // digitalWrite(5, digitalRead(6));
       timeThis = millis();
     }
     digitalWrite(5, HIGH);
     pinMode(8, INPUT);
-
+/*
     delay(1000);
     timeThis = millis();
     timeLast = timeThis;
@@ -51,7 +64,7 @@ void loop() {
     }
     digitalWrite(5, HIGH);
     //  pinMode(8, INPUT);
-  }
+  }*/
 
 
 
