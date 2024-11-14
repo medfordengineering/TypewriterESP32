5783897e142185879e806c60594a35d7d0921a76
medfordengineering <schristy@medford.k12.ma.us>
2024-11-14 15:27:46 -0500

    added array to store and write letters

--- TypewriterESP32/TypewriterESP32.ino
+++ TypewriterESP32/TypewriterESP32.ino
@@ -1,10 +1,22 @@
 
 #include <Adafruit_MCP23X17.h>
 
+#define set_bit(var, pin) var |= 1 << (unsigned char)pin
+#define clear_bit(var, pin) var &= ~(1 << (unsigned char)pin)
+#define clr_bit(var, pin) clear_bit(var, pin)
+#define test_bit(var, pin) ((var & (1 << (unsigned char)pin)) >> pin)
+#define toggle_bit(var, pin) var ^= 1 << (unsigned char)pin
+
 Adafruit_MCP23X17 mcp;
 
 unsigned long timeThis;
 unsigned long timeLast;
+
+uint8_t letters[10][2] = {
+  { 0, 0 },  //5
+  { 1, 0 },  //u
+  { 2, 0 }   //o
+};
 
 void setup() {
   Serial.begin(9600);
@@ -21,16 +33,18 @@ void setup() {
     mcp.pinMode(x, OUTPUT);
     mcp.digitalWrite(x, HIGH);
   }
-  mcp.digitalWrite(0, LOW);
-  mcp.digitalWrite(1, LOW);
-  mcp.digitalWrite(2, LOW);
-  mcp.digitalWrite(7, HIGH);
+  for (int i = 0; i < 3; i++) {
+    if (test_bit(letters[2][0], i))
+      mcp.digitalWrite(i, HIGH);
+    else
+      mcp.digitalWrite(i, LOW);
+  }
 }
 
 void loop() {
 
   timeThis = millis();
-  if (timeThis - timeLast >= 200) {
+  if (timeThis - timeLast >= 1000) {
     mcp.digitalWrite(7, LOW);
     delay(50);
     timeLast = timeThis;
