417ca3a403e4d5064d36100a46040a84d78e0cf7
medfordengineering <schristy@medford.k12.ma.us>
2024-11-21 14:14:53 -0500

    Cascading with the 74HC151

--- TypewriterControlTest/TypewriterControlTest.ino
+++ TypewriterControlTest/TypewriterControlTest.ino
@@ -164,14 +164,19 @@ uint8_t letters[128][2] = {
 void send_character(uint8_t c) {
   uint8_t tx_pin_select = letters[c][0];
   uint8_t rx_pin_select = letters[c][1];
-
+  /*
   if (rx_pin_select > 7) {
     rx_pin_select -= 8;
     rx_pin_select <<= 4;
     rx_pin_select |= 0x08;
   } else {
     rx_pin_select |= 0x80;
-  }
+  }*/
+
+  rx_pin_select |= 0x70;
+
+  Serial.println(rx_pin_select, HEX);
+
   //tx_pin_select |= 0x08;
   mcp.writeGPIOA(rx_pin_select);
   mcp.writeGPIOB(tx_pin_select);
@@ -192,31 +197,18 @@ void setup() {
       ;
   }
 
-  for (int x =
-         0;
-       x < 16; x++) {
+  for (int x = 0; x < 16; x++) {
     mcp.pinMode(x, OUTPUT);
     mcp.digitalWrite(x, HIGH);
   }
-
-  // mcp.writeGPIOB(6);
-  //while(1);
-  //send_character(2);
-
-  /*
-  uint8_t letter = letters[2][1] << 3 | letters[2][0];
-  Serial.println(letter, BIN);
-  for (int i = 0; i < 6; i++) {
-    // if (test_bit(letters[2][0], i))
-    if (test_bit(letter, i))
-      mcp.digitalWrite(i, HIGH);
-    else
-      mcp.digitalWrite(i, LOW);
-  }*/
 }
 
 void loop() {
-  send_character('h');
+  char test[20] = "ghijk";
+  for (int x = 0; x < 4; x++) {
+    send_character(test[x]);
+  }
+  //send_character('g');
   delay(2000);
   /*
   timeThis = millis();
